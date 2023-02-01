cd4aac22bfe526e2ff7a9d5c5e81834a54aeeb2b
Wiktor <Wiktor@DESKTOP-CPOS1R4>
2023-02-01 20:30:29 +0100

    Zadanie domowe lekcja 4

--- challengapp1/challengapp1/Program.cs
+++ challengapp1/challengapp1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var gender = "Kobieta";
+var age = 20;
+string name = "Ewa";
+
+if (gender == "kobieta" && age < 30)
+{
+
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (gender != "kobieta" && age > 18)
+{
+    Console.WriteLine("Niepelnoletni mężczyzna");
+}
+else if (name=="Ewa"||gender!="kobieta")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
