ed6f50632afe33ddee355e4ec7843cc9a73ee4ac
Wiktor <Wiktor@DESKTOP-CPOS1R4>
2023-02-02 20:10:09 +0100

    Zadanie domowe dzień 5

--- challengapp1/challengapp1/Program.cs
+++ challengapp1/challengapp1/Program.cs
@@ -1,20 +1,54 @@
-﻿var gender = "Kobieta";
-int age = 20;
-string name = "Ewa";
+﻿int number = 19950412;
+    string numberInString = number.ToString ();
+char[] letters = numberInString.ToArray();
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+foreach(char letter in letters)
+{
+    if (letter == '0')
+    { counter0++; }
+    else if (letter == '1')
+    { counter1++; }
+else if (letter == '2')
+    { counter2++;}
+    else if(letter=='3')
+    { counter3++; }
+    else if(letter=='4')
+    { counter4++; }
+    else if(letter=='5')
+    { counter5++; }
+    else if(letter=='6')
+    { counter6++; }
+    else if (letter=='7')
+    { counter7++; }
+    else if(letter=='8')
+    { counter8++; }
+    else if(letter=='9')
+    { counter9++; }
+    
+}
 
-if (age >= 20)
-{
 
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (gender != "kobieta" && age > 18)
-{
-    Console.WriteLine("Niepelnoletni mężczyzna");
-}
-else if (name != "Ewa" | age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
+Console.WriteLine("Moja liczba to: " + number);
+Console.WriteLine("0 ==> " + counter0);
+Console.WriteLine("1 ==> " + counter1);
+Console.WriteLine("2 ==> " + counter2);
+Console.WriteLine("3 ==> " + counter3);
+Console.WriteLine("4 ==> " + counter4);
+Console.WriteLine("5 ==> " + counter5);
+Console.WriteLine("6 ==> " + counter6);
+Console.WriteLine("7 ==> " + counter7);
+Console.WriteLine("8 ==> " + counter8);
+Console.WriteLine("9 ==> " + counter9);
+
 
 
 
