aa6a29ff3fa639562f6c55f0e706c04f3a44b99c
Wiktor <Wiktor@DESKTOP-CPOS1R4>
2023-02-06 19:21:06 +0100

    Zadanie domowe dzień 6

--- challengapp1/challengapp1/Program.cs
+++ challengapp1/challengapp1/Program.cs
@@ -1,53 +1,50 @@
-﻿int number = 19950412;
-    string numberInString = number.ToString ();
-char[] letters = numberInString.ToArray();
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
-foreach(char letter in letters)
+﻿var User1 = new User("Wiktor","Kwilosz",27);
+User1.AddScore(5);
+User1.AddScore(6);
+User1.AddScore(9);
+User1.AddScore(7);
+User1.AddScore(4);
+
+var User2 = new User("Jan","Kowalski",20);
+User2.AddScore(3);
+User2.AddScore(5);
+User2.AddScore(4);
+User2.AddScore(9);
+User2.AddScore(8);
+
+var User3 = new User("Ewa","Kowalska",19);
+User3.AddScore(1);
+User3.AddScore(2);
+User3.AddScore(3);
+User3.AddScore(4);
+User3.AddScore(5);
+
+List<User> Users =new List<User>()
 {
-    if (letter == '0')
-    { counter0++; }
-    else if (letter == '1')
-    { counter1++; }
-else if (letter == '2')
-    { counter2++;}
-    else if(letter=='3')
-    { counter3++; }
-    else if(letter=='4')
-    { counter4++; }
-    else if(letter=='5')
-    { counter5++; }
-    else if(letter=='6')
-    { counter6++; }
-    else if (letter=='7')
-    { counter7++; }
-    else if(letter=='8')
-    { counter8++; }
-    else if(letter=='9')
-    { counter9++; }
-    
+    User1, User2, User3
+};
+int MaxResult = -1;
+User UsersWithMaxResult = null;
+
+foreach (var user in Users)
+{
+    if (user.Result > MaxResult)
+    {
+        MaxResult = user.Result;
+                UsersWithMaxResult = user;
+    }
+
 }
+Console.WriteLine("Pracownik z najwyższą ilością punktów to:" + UsersWithMaxResult.Name + " " + UsersWithMaxResult.Surname + " " + "Lat" + UsersWithMaxResult.Age);
+Console.WriteLine("Suma punktów:" + UsersWithMaxResult.Result);
 
 
-Console.WriteLine("Moja liczba to: " + number);
-Console.WriteLine("0 ==> " + counter0);
-Console.WriteLine("1 ==> " + counter1);
-Console.WriteLine("2 ==> " + counter2);
-Console.WriteLine("3 ==> " + counter3);
-Console.WriteLine("4 ==> " + counter4);
-Console.WriteLine("5 ==> " + counter5);
-Console.WriteLine("6 ==> " + counter6);
-Console.WriteLine("7 ==> " + counter7);
-Console.WriteLine("8 ==> " + counter8);
-Console.WriteLine("9 ==> " + counter9);
+
+  
+
+
+
+
 
 
 
---
+++ challengapp1/challengapp1/User.cs
@@ -0,0 +1,33 @@
+﻿
+public class User
+{
+   
+
+    private List<int> scores = new List<int>();
+
+    public User(string Name, string Surname, int Age)
+    {
+        this.Name = Name;
+        this.Surname = Surname;
+        this.Age = Age;
+    }
+
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    public int Age { get; private set; }
+    public int Result
+
+
+    {
+        get
+        {
+            return this.scores.Sum();
+
+        }
+    }
+    public void AddScore(int newscores)
+    {
+        this. scores.Add(newscores);
+    }
+    
+}
